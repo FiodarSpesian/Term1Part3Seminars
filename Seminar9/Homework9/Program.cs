14f699c37004ddfb79545cbe11cce01044e952f8
Fiodar <fiodarspesian@mail.ru>
2022-12-10 18:04:30 +0300

    decide sem9

--- Seminar9/Homework9/Program.cs
+++ Seminar9/Homework9/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+void Case64()
+{
+    Console.Write("Enter number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    Console.WriteLine($"Your number {number}, all digits till 1: ");
+    OutNums(number);
+}
+Case64();
+void OutNums(int number)
+{
+    if(number == 0) return;
+    Console.Write(number + " ");
+    number --;
+    OutNums(number);
+}
+
+
+
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+
+
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
